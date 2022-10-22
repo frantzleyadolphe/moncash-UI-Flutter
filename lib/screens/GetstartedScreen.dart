0ef04e41eb65dc1103495f88b1c3bbb0ed10ddc4
pirate23 <dolphdjyangelo@gmail.com>
2022-10-22 12:05:59 -0400

    M retire body an deyo klas lan m metel aandan epi retire widget kem t gen an

--- lib/screens/GetstartedScreen.dart
+++ lib/screens/GetstartedScreen.dart
@@ -21,7 +21,7 @@ class GetStartedScreen extends StatelessWidget {
                 style: TextStyle(color: Color(0xFFD60101), fontSize: 18),
               ),
               onPressed: () {
-                Navigator.push(
+                Navigator.pushReplacement(
                     context,
                     MaterialPageRoute(
                         builder: (context) => const LoginScreen()));
@@ -36,71 +36,69 @@ class GetStartedScreen extends StatelessWidget {
           const SizedBox(height: 20),
           descSection,
           const SizedBox(height: 40),
-          buttonSection
+          Container(
+            height: 50,
+            width: 50,
+            alignment: Alignment.bottomCenter,
+            decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 4,
+                  offset: Offset(0, 4),
+                )
+              ],
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LoginScreen()));
+              },
+              style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(10),
+                  backgroundColor: const Color(0xFFD60101)),
+              child: SvgPicture.asset('icons/arrowRight.svg',
+                  color: Colors.white, height: 50, width: 50),
+            ),
+          )
         ],
       ),
     );
   }
 }
 
+//widget pou image svg an
 Widget svgSection = Container(
   height: 400,
   padding: const EdgeInsets.only(right: 10, left: 10),
   margin: const EdgeInsets.only(right: 10, left: 10),
   child: const Image(image: AssetImage('images/wallet.png')),
 );
-
+//widget pou pati welcome nan
 Widget welcomeSection = const SizedBox(
   child: Center(
-   child: SizedBox(
-     width: 250,
-     child: Text("WELCOME\nTO MONCASH",
-     textAlign: TextAlign.center,
-     style: TextStyle(
-                 fontFamily: 'Poppins',
-                 fontSize: 30,
-                 color: Color(0xFFD60101))),
-   ),
-      ),
-);
-
-Widget descSection = const SizedBox(
-  width: 250,
-  child: Center(
-        child: Text("Pay with your e-wallet and make your withdrawals and deposits anywhere in Haiti with an authorized agent",
+    child: SizedBox(
+      width: 250,
+      child: Text("WELCOME\nTO MONCASH",
           textAlign: TextAlign.center,
           style: TextStyle(
-              fontSize: 13,
-              fontFamily: 'PoMedium',
-              color: Color(0xFFD60101)),
-        ),
-      )
-);
-
-Widget buttonSection = Container(
-  height: 50,
-  width: 50,
-  alignment: Alignment.bottomCenter,
-  decoration: const BoxDecoration(
-    boxShadow: [
-      BoxShadow(
-        color: Colors.grey,
-        blurRadius: 4,
-        offset: Offset(0, 4),
-      )
-    ],
-    borderRadius: BorderRadius.all(Radius.circular(25)),
-  ),
-  child: ElevatedButton(
-    onPressed: () {
-      //Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
-    },
-    style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(),
-        padding: const EdgeInsets.all(10),
-        backgroundColor: const Color(0xFFD60101)),
-    child: SvgPicture.asset('icons/arrowRight.svg',
-    color: Colors.white, height: 50, width: 50),
-    
+              fontFamily: 'Poppins', fontSize: 30, color: Color(0xFFD60101))),
+    ),
   ),
 );
+//widget pou pati description an
+Widget descSection = const SizedBox(
+    width: 250,
+    child: Center(
+      child: Text(
+        "Pay with your e-wallet and make your withdrawals and deposits anywhere in Haiti with an authorized agent",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            fontSize: 13, fontFamily: 'PoMedium', color: Color(0xFFD60101)),
+      ),
+    ));
+//widget pou pati bouton an
