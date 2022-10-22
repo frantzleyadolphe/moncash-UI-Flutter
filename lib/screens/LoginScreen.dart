04988567ec363fba9fcd12928b4c06b1961eb50e
pirate23 <dolphdjyangelo@gmail.com>
2022-10-22 12:11:34 -0400

    Gerer probleme bouton m retire widget ki t deyo an m mete l anndan klas lan du coup m vinn pa gen problem ak context lan

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: file_names
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'package:moncash/screens/GetstartedScreen.dart';
-//import 'package:moncash/screens/HomeScreen.dart';
+import 'package:moncash/screens/HomeScreen.dart';
 import 'package:moncash/styles/colors.dart';
 import 'package:moncash/styles/fontStyles.dart';
 
@@ -25,7 +23,107 @@ class _LoginScreenState extends State<LoginScreen> {
               imageSection,
               textSection,
               const SizedBox(height: 10),
-              formSection
+              //widget pou pati formulaire lan
+              Container(
+                height: 400,
+                //color: Colors.black,
+                margin: const EdgeInsets.only(right: 20, left: 20),
+                //color: Color(0xFFD60101),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 53,
+                      decoration: BoxDecoration(
+                        color: fieldColor,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter your phone number',
+                            contentPadding: EdgeInsets.all(10),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      height: 53,
+                      decoration: BoxDecoration(
+                        color: fieldColor,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Enter your pin code',
+                            contentPadding: EdgeInsets.all(10),
+                            border: InputBorder.none),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      height: 20,
+                      child: Text("Forgot your pin ?", style: sousTitre2),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    //widget pu bouton login lan
+                    TextButton(
+                        style: TextButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)),
+                          backgroundColor: primaryColor,
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 11,
+                            horizontal: 105,
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+                        },
+                        child: Text("Login", style: buttonText)),
+                    const SizedBox(height: 15),
+                    Container(
+                      alignment: Alignment.center,
+                      height: 20,
+                      //color: Colors.blue,
+                      child: Text("Or", style: sousTitre3),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox.fromSize(
+                      size: const Size(56, 56),
+                      child: ClipOval(
+                        child: Material(
+                          color: fieldColor,
+                          child: InkWell(
+                            splashColor: primaryColor,
+                            onTap: () {},
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.face_retouching_natural_rounded,
+                                    color: primaryColor), // <-- Icon
+                                // <-- Text
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextButton(
+                        onPressed: () {
+                          //kod ki ap banm prob lan
+                          //Navigator.push(context,
+                          //MaterialPageRoute(builder: (ctx) => const GetStartedScreen()));
+                        },
+                        child: Text(
+                          "Haven’t account ? Sign Up Here",
+                          style: sousTitre4,
+                        ))
+                  ],
+                ),
+              )
             ],
           ),
         ));
@@ -60,107 +158,5 @@ Widget textSection = Container(
         )
       ],
     ),
-  ),
-);
-
-Widget formSection = Container(
-  height: 400,
-  //color: Colors.black,
-  margin: const EdgeInsets.only(right: 20, left: 20),
-  //color: Color(0xFFD60101),
-  child: Column(
-    children: [
-      Container(
-        height: 53,
-        decoration: BoxDecoration(
-          color: fieldColor,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: const TextField(
-          decoration: InputDecoration(
-              hintText: 'Enter your phone number',
-              contentPadding: EdgeInsets.all(10),
-              border: InputBorder.none),
-        ),
-      ),
-      const SizedBox(height: 20),
-      Container(
-        height: 53,
-        decoration: BoxDecoration(
-          color: fieldColor,
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: const TextField(
-          decoration: InputDecoration(
-              hintText: 'Enter your pin code',
-              contentPadding: EdgeInsets.all(10),
-              border: InputBorder.none),
-        ),
-      ),
-      const SizedBox(height: 10),
-      Container(
-        alignment: Alignment.centerRight,
-        height: 20,
-        child: Text("Forgot your pin ?", style: sousTitre2),
-      ),
-      const SizedBox(
-        height: 8,
-      ),
-      TextButton(
-          style: TextButton.styleFrom(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-            backgroundColor: primaryColor,
-            padding: const EdgeInsets.symmetric(
-              vertical: 11,
-              horizontal: 105,
-            ),
-          ),
-          onPressed: () {
-            //Navigator.push(context,
-               // MaterialPageRoute(builder: (context) => const HomeScreen()));
-          },
-          child: Text("Login", style: buttonText)),
-      const SizedBox(height: 15),
-      Container(
-        alignment: Alignment.center,
-        height: 20,
-        //color: Colors.blue,
-        child: Text("Or", style: sousTitre3),
-      ),
-      const SizedBox(height: 20),
-      SizedBox.fromSize(
-        size: const Size(56, 56),
-        child: ClipOval(
-          child: Material(
-            color: fieldColor,
-            child: InkWell(
-              splashColor: primaryColor,
-              onTap: () {},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.face_retouching_natural_rounded,
-                      color: primaryColor), // <-- Icon
-                  // <-- Text
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-      const SizedBox(height: 10),
-      TextButton(
-          onPressed: () {
-            //kod ki ap banm prob lan
-
-            //Navigator.push(context,
-            //MaterialPageRoute(builder: (ctx) => const GetStartedScreen()));
-          },
-          child: Text(
-            "Haven’t account ? Sign Up Here",
-            style: sousTitre4,
-          ))
-    ],
   ),
 );
