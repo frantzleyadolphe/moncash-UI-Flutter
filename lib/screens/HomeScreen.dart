84778add17245cab68a55ab7bdb0dd921595b5f0
pirate23 <dolphdjyangelo@gmail.com>
2022-10-21 20:46:23 -0400

    page HomeScreen added

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Container()),
+    );
+  }
+}
